10f6981518951bebcbf06934eb2df2a42d5cc413
Sakina Shivji <shivjisakina@gmail.com>
2017-08-19 13:55:58 -0400

    got ajax request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
   // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/shivjisakina.json',
+        dataType: 'jsonp',
+        success: function(response) {
+             console.log(response.badges[i])
+
+         }
+    });
 
 });
